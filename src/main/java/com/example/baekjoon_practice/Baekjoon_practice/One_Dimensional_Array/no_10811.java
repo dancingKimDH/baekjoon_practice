7012302df22f13254b15c57711ae9c4530668f24
dancingkimDH <kimdonghyun0701@gmail.com>
2023-10-23 17:19:46 +0900

    update

--- src/main/java/com/example/baekjoon_practice/Baekjoon_practice/One_Dimensional_Array/no_10811.java
+++ src/main/java/com/example/baekjoon_practice/Baekjoon_practice/One_Dimensional_Array/no_10811.java
@@ -9,21 +9,25 @@ public class no_10811 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] group = new int[N];
-        int[] aftergroup = new int[N];
 
         for(int a = 0; a < group.length; a ++) {
             group[a] = a + 1;
-            aftergroup[a] = a + 1;
+        }
+        int t = 0;
+        int M = sc.nextInt();
+        for(int a = 0; a <= M; a ++) {
+            int i = sc.nextInt(); 
+            int j = sc.nextInt();
+            for(int x = i - 1; x < j - 1; x ++ ){
+                t = group[i - 1];
+                group[i - 1] = group[j - x - 1];
+                group[j - x - 1] = t;
+                System.out.println("round completed");
+            }
         }
 
-        int M = sc.nextInt();
-
-        for(int a = 0; a <= M; a ++) {
-            int i = sc.nextInt();
-            int j = sc.nextInt();
-            for(int z = i - 1; z < j; z ++) {
-                aftergroup[z] = group[j-1];
-            }
+        for(int a = 0; a < group.length; a ++) {
+            System.out.println(group[a]);
         }
 
     }
